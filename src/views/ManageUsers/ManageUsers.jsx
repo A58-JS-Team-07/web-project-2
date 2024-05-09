3e9b6ed28b9f9f0524381fb7e69620d84500b933
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-09 13:05:55 +0300

    Complete Manage users functionality

--- src/views/ManageUsers/ManageUsers.jsx
+++ src/views/ManageUsers/ManageUsers.jsx
@@ -1,23 +1,45 @@
 import {
   getAllUsers,
   changeAdminStatus,
-  changeBanStatus
+  changeBanStatus,
 } from "../../services/users.service.js";
 import Button from "../../components/Button/Button.jsx";
 import ProfileAvatar from "../../components/Header/Profile/ProfileAvatar.jsx";
 import { useEffect, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import { useContext } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function ManageUsers() {
   const [users, setUsers] = useState([]);
   const { userData } = useContext(AppContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const search = searchParams.get("search") || "";
+
+  const setSearch = (value) => {
+    setSearchParams({ search: value });
+  };
 
   useEffect(() => {
     getAllUsers().then((snapshot) => {
-      setUsers(Object.values(snapshot.val()));
+      const users = Object.values(snapshot.val());
+      if (search) {
+        setUsers(
+          users.filter((user) => {
+            return user.username.includes(search) || user.email.includes(search);
+          })
+        );
+      } else {
+        setUsers(users);
+      }
     });
-  }, []);
+  }, [search]);
+
+  // useEffect(() => {
+  //   getAllUsers().then((snapshot) => {
+  //     setUsers(Object.values(snapshot.val()));
+  //   });
+  // }, []);
 
   function handleMakeAdmin(user) {
     const currStatus = user.isAdmin;
@@ -32,7 +54,6 @@ function ManageUsers() {
       });
     });
   }
-
 
   function handleBanUser(user) {
     const currStatus = user.isBanned;
@@ -51,36 +72,61 @@ function ManageUsers() {
   return (
     <div>
       <h1>Manage Users</h1>
-      {users.filter((user) => user.uid !== userData.uid).map((user) => {
-        return (
-          <div className="user-card" key={user.uid}>
-            <div className="user-card__info">
-              <div className="user-card__info--left">
-                {/*TODO: Add condition for user with avatar*/}
-                <ProfileAvatar />
+      <label htmlFor="search">Search</label>
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search users by username or email"
+        name="search"
+        type="text"
+        id="search"
+      />
+      {users
+        .filter((user) => user.uid !== userData.uid)
+        .map((user) => {
+          return (
+            <div className="user-card" key={user.uid}>
+              <div className="user-card__info">
+                <div className="user-card__info--left">
+                  {/*TODO: Add condition for user with avatar*/}
+                  <ProfileAvatar />
+                </div>
+                <div className="user-card__info--right">
+                  <span className="user-card__info__username">
+                    {user.username}
+                  </span>
+                  <span className="user-card__info__email">{user.email}</span>
+                </div>
               </div>
-              <div className="user-card__info--right">
-                <span className="user-card__info__username">
-                  {user.username}
-                </span>
-                <span className="user-card__info__email">{user.email}</span>
+              <div className="user-card__actions">
+                {user.isAdmin ? (
+                  <Button
+                    onClick={() => handleMakeAdmin(user)}
+                    variant="danger"
+                  >
+                    Remove admin
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={() => handleMakeAdmin(user)}
+                    variant="primary"
+                  >
+                    Make admin
+                  </Button>
+                )}
+                {user.isBanned ? (
+                  <Button onClick={() => handleBanUser(user)} variant="danger">
+                    Unban
+                  </Button>
+                ) : (
+                  <Button onClick={() => handleBanUser(user)} variant="primary">
+                    Ban
+                  </Button>
+                )}
               </div>
             </div>
-            <div className="user-card__actions">
-              {user.isAdmin ? (
-                <Button onClick={() => handleMakeAdmin(user)} variant="danger">Remove admin</Button>
-              ) : (
-                <Button onClick={() => handleMakeAdmin(user)} variant="primary">Make admin</Button>
-              )}
-              {user.isBanned ? (
-                <Button onClick={() => handleBanUser(user)} variant="danger">Unban</Button>
-              ) : (
-                <Button onClick={() => handleBanUser(user)} variant="primary">Ban</Button>
-              )}
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
