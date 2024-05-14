ce0fc3b48e648916bcf56bf46b1cb1ff99220bd0
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-14 19:19:48 +0300

    Styling register page

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -39,12 +39,21 @@ function Register() {
         alert("Username already exists");
         return;
       }
-      if (!form.email || !form.password || !form.username, !form.firstName, !form.lastName) {
+      if (
+        (!form.email || !form.password || !form.username,
+        !form.firstName,
+        !form.lastName)
+      ) {
         alert("Please fill in all fields");
         return;
       }
-      
-      if (form.firstName.length < 4 || form.firstName.length > 32 || form.lastName.length < 4 || form.lastName.length > 32) {
+
+      if (
+        form.firstName.length < 4 ||
+        form.firstName.length > 32 ||
+        form.lastName.length < 4 ||
+        form.lastName.length > 32
+      ) {
         alert("First and last name must be between 4 and 32 characters");
         return;
       }
@@ -64,6 +73,9 @@ function Register() {
       if (error.code === "auth/email-already-in-use") {
         alert("Email already in use");
         return;
+      } else if (error.code === "auth/invalid-email") {
+        alert("Invalid email format");
+        return;
       } else {
         alert("An error occurred");
         console.error(error);
@@ -74,65 +86,67 @@ function Register() {
   return (
     <div className="register">
       <h1>Register</h1>
-      <div className="register__row">
-        <div className="register__form-group">
-          <label htmlFor="firstName">First name</label>
-          <input
-            value={form.firstName}
-            onChange={updateForm("firstName")}
-            type="text"
-            id="firstName"
-            name="firstName"
-          />
+      <div className="register__form">
+        <div className="register__row">
+          <div className="register__form-group">
+            <label htmlFor="firstName">First name</label>
+            <input
+              value={form.firstName}
+              onChange={updateForm("firstName")}
+              type="text"
+              id="firstName"
+              name="firstName"
+            />
+          </div>
+          <div className="register__form-group">
+            <label htmlFor="lastName">Last name</label>
+            <input
+              value={form.lastName}
+              onChange={updateForm("lastName")}
+              type="text"
+              id="lastName"
+              name="lastName"
+            />
+          </div>
         </div>
-        <div className="register__form-group">
-          <label htmlFor="lastName">Last name</label>
-          <input
-            value={form.lastName}
-            onChange={updateForm("lastName")}
-            type="text"
-            id="lastName"
-            name="lastName"
-          />
+        <div className="register__row">
+          <div className="register__form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={form.username}
+              onChange={updateForm("username")}
+              type="text"
+              id="username"
+              name="username"
+            />
+          </div>
         </div>
+        <div className="register__row">
+          <div className="register__form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              value={form.email}
+              onChange={updateForm("email")}
+              type="email"
+              id="email"
+              name="email"
+            />
+          </div>
+        </div>
+        <div className="register__row">
+          <div className="register__form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              value={form.password}
+              onChange={updateForm("password")}
+              type="password"
+              id="password"
+              name="password"
+            />
+          </div>
+        </div>
+        <Button onClick={register}>Register</Button>
       </div>
-      <div className="register__row">
-        <div className="register__form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            value={form.username}
-            onChange={updateForm("username")}
-            type="text"
-            id="username"
-            name="username"
-          />
-        </div>
-      </div>
-      <div className="register__row">
-        <div className="register__form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            value={form.email}
-            onChange={updateForm("email")}
-            type="email"
-            id="email"
-            name="email"
-          />
-        </div>
-      </div>
-      <div className="register__row">
-        <div className="register__form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            value={form.password}
-            onChange={updateForm("password")}
-            type="password"
-            id="password"
-            name="password"
-          />
-        </div>
-      </div>
-      <Button onClick={register}>Register</Button>
     </div>
   );
 }
